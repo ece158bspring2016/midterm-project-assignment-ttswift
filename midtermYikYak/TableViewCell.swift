96e8b4054e24306cc6669fb2e74fd829475a1125
Pablo Volpe <9493801704>
2016-04-22 17:26:34 -0700

    initial, setting up table view cell

---
+++ midtermYikYak/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  midtermYikYak
+//
+//  Created by Pablo Volpe on 4/22/16.
+//  Copyright © 2016 Pablo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
